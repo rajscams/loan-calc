fbc6c97b5730b59ddfd84b8df19c2e239f9a4ac5
rajscams <117559077+rajscams@users.noreply.github.com>
2022-11-06 00:38:21 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import LoanCalculator from './LoanCalculator';
+ 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LoanCalculator />
+      </header>
+    </div>
+  );
+}
+ 
+export default App;
